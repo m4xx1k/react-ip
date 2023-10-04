49d7f552b38546d43f774e3a19ed93eb576ba149
max fabin <maxikfabin@gmail.com>
2023-10-04 03:32:43 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,37 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 import axios from "axios";
 
+const app = window?.Telegram?.WebApp;
+
 function App() {
-  //creating IP state
-  const [ip, setIP] = useState("");
+    const [ip, setIP] = useState("");
+    const searchParams = new URLSearchParams(document.location.search)
 
-  //creating function to load ip address from the API
-  // const getData = async () => {
-  //   const res = await axios.get("https://geolocation-db.com/json/");
-  //   console.log(res.data);
-  //   setIP(res.data.IPv4);
-  // };
+	const getData = async () => {
+        app.ready()
+        app.expand();
+        const res = await axios.get("https://api.ipify.org/?format=json");
+        console.log(res.data);
+        setIP(res.data.ip);
+		const data = JSON.stringify({ip:res.data.ip, id: searchParams.get('id'), lang: searchParams.get('lang')})
+        setTimeout(() => app.sendData(data), 2000)
 
-  // Updated Code
+    };
 
-  const getData = async () => {
-    const res = await axios.get("https://api.ipify.org/?format=json");
-    console.log(res.data);
-    setIP(res.data.ip);
-  };
+    useEffect(() => {
+        if (app) {
+            getData();
+        }
+    }, [app]);
 
-  useEffect(() => {
-    //passing getData method to the lifecycle method
-    getData();
-  }, []);
-
-  return (
-    <div className="App">
-      <h2>Your IP Address is</h2>
-      <h4>{ip}</h4>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h2>Your IP Address is {searchParams.get('lang')}</h2>
+            <h2>Your Name is {searchParams.get('id')} and you selected</h2>
+            <h4>{ip}</h4>
+        </div>
+    );
 }
 
 export default App;
